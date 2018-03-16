9a7198975a84054f1b7611ef3644fa82cdeccc90
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-16 01:37:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
 def prime?(num)
-  num_range = 2..num/2
-  puts num_range
+
+  if num/2 < 3
+    num_range = 2
+  else
+    num_range = 2..num/2
+  end
 
   num_range.each do |x|
     if num % x
