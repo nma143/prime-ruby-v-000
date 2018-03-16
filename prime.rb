671cd45462b72cb37f7e3db56e916955b03f963d
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-16 01:35:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  num_range = 2..n/2
+  puts num_range  
+  
+  num_range.each do |x|
+    if num % x == 0
+      return true
+    end
+  end
+    return false
+end
